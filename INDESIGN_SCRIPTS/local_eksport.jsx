8c56b8c01f42e0e05877bcbe36bb329cc7055fee
Håken Lid <haakenlid@gmail.com>
2014-08-22 19:18:48 +0200

    Skript for å lage thumbs fra pdf.

--- INDESIGN_SCRIPTS/local_eksport.jsx
+++ INDESIGN_SCRIPTS/local_eksport.jsx
@@ -1,11 +1,11 @@
 ﻿var scriptFile = "eksportSider.jsx";
-var scriptFolder = "/Volumes/univ-desken/UTTEGNER/SCRIPTS_CS55/"
+var scriptFolder = "/Volumes/univ-desken/UTTEGNER/SCRIPTS_CS55/";
 
 var myRunScript = function (scriptFile, scriptFolder){
 	var myScript = File(scriptFolder+scriptFile);
 	if (myScript.exists){
 		app.doScript (myScript);
 	}
-}
+};
 
 myRunScript(scriptFile, scriptFolder);
