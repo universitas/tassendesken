2f37415b8e4579fb5e45ac2fc82b3164bd73c74a
Håken Lid <haakenlid@gmail.com>
2014-11-21 19:08:02 +0100

    logging for collection

--- CRON/collect_images_and_pdfs.sh
+++ CRON/collect_images_and_pdfs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# IFS=$'\n'
+function log {
+    while IFS= read -r line; do
+        echo "$(date +"[%Y-%m-%d %H:%m:%S]") $line" >> $1
+    done
+}
+
+IFS=$'\n'
 # PATH="/usr/bin:/bin:$SCRIPT_FOLDER"
 DESKEN="/uio/kant/div-universitas-desken"
 
@@ -9,10 +15,10 @@ STAGING="$DESKEN/STAGING"
 IMAGE_FOLDER="$STAGING/IMAGES"
 PDF_FOLDER="$STAGING/PDF"
 
-remote_linode="haakenlid@linode:/srv/fotoarkiv_universitas/STAGING/"
-remote_domeneshop="universitas@login.domeneshop.no:static/bilder/"
+remote_linode="haakenlid@universitas.no:/srv/fotoarkiv_universitas"
+remote_domeneshop="universitas@login.domeneshop.no:static/bilder"
 
-logfile="rsync.log"
+logfile="$STAGING/collect.log"
 
 if [ "$2" ]; then
   YEAR=$1
@@ -22,7 +28,7 @@ else
   ISSUE=$(ls $DESKEN/ | grep -e '^[0-9]\{1,3\}$' | sort -nr | head -n 1)
 fi
 
-echo "$ISSUE ($YEAR)"
+echo "Collecting images and pdfs for: $ISSUE ($YEAR)" | log $logfile
 
 if [ "$ISSUE" = "" ]; then
   exit
@@ -40,15 +46,13 @@ ERROR='_ERROR_'
 image_files=$(find "$DESKEN/$ISSUE" -iregex '.*\.\(jpg\|jpeg\|png\)' ! -name '._*' ! -name "$ERROR*")
 for original in $image_files; do
   original=$($SCRIPT_FOLDER/fix_filnavn.py $original)
-  # ln -s $image $IMAGE_FOLDER
   compressed="$IMAGE_FOLDER/$(basename $original)"
   if [[ ! -f "$compressed" || "$original" -nt "$compressed" ]]; then
     convert "$original" -resize 1500x -quality 60 "$compressed"
-    #echo "compressed  $original"
-    #echo "        ->  $compressed"
+    echo "compressed  $original" | log $logfile
     if [[ ! -f "$compressed" ]]; then
       broken_file=$(dirname "$original")"/$ERROR"$(basename "$original")
-      echo "ERROR:   " $broken_file
+      echo "ERROR: " $broken_file | log $logfile
       mv $original $broken_file
     fi
   fi
@@ -58,6 +62,7 @@ done
 for compressed in $(ls $IMAGE_FOLDER); do
   if [[ "" == $(find "$DESKEN/$ISSUE" -name "$compressed") ]]; then
     rm "$IMAGE_FOLDER/$compressed"
+    echo "removed $compressed" | log $logfile
   fi
 done
 
@@ -67,14 +72,12 @@ for pdf_file in $pdf_files; do
   ln -s $pdf_file $PDF_FOLDER
 done
 
-# key="/uio/kant/div-universitas-u1/haakenl/.ssh/id_rsa"
-
 # uploading to domeneshop
-logg="$STAGING/domeneshop-rsync.log"
+logfile="$STAGING/domeneshop-rsync.log"
 remote="$remote_domeneshop/$YEAR/$ISSUE"
-/usr/bin/rsync -rthzv $IMAGE_FOLDER $remote > "$STAGING/$logg"
+/usr/bin/rsync -rthzv $IMAGE_FOLDER $remote | log $logfile
 
 # uploading to linode
-logg="$STAGING/linode-rsync.log"
+logfile="$STAGING/linode-rsync.log"
 remote="$remote_linode"
-/usr/bin/rsync -rthzv $STAGING_FOLDER $remote > "$STAGING/$logg"
+/usr/bin/rsync -rthzvL $STAGING $remote --chmod="ug+rw" --delete --exclude=*.log | log $logfile
