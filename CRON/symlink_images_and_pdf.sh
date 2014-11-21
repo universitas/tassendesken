61637c5299b9b977a3c788c2a8491ad221c31e47
Håken Lid <haakenlid@gmail.com>
2014-11-21 13:24:19 +0100

    merge changes

---
+++ CRON/symlink_images_and_pdf.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+DESKEN="/uio/kant/div-universitas-desken"
+IMAGE_FOLDER="$DESKEN/SCRIPTS/CRON/STAGING/IMAGES"
+PDF_FOLDER="$DESKEN/SCRIPTS/CRON/STAGING/PDF"
+
+cd $DESKEN
+
+if [ "$2" ]; then
+  aar=$1
+  avisnr=$2
+else
+  aar=$(date +%Y)
+  avisnr=$(ls $PLATON/ | grep -e '^[0-9]\{1,3\}$' | sort -nr | head -n 1)
+fi
+echo "$avisnr ($aar)"
+
+if [ "$avisnr" = "" ]; then
+  exit
+fi
+
+PATH="/usr/bin:/bin"
+IFS=$'\n'
+
+# Check that we are not already running
+touch lock
+read last_pid < lock
+[ ! -z "$last_pid" -a -d /proc/$last_pid ] && echo "Already running" && exit
+echo $$ > lock
+
+# Symlink jpg and png files.
+image_files = $(find "$DESKEN/$avisnr" -iname "*.jpg" -and -not -path "*/._*" -or -iname "*.png" -and -not -path "*/._*" )
+pdf_files = $(find "$DESKEN/$avisnr" -iname "UNIV*.pdf")
+
+for image in $image_files do;
+  # fix_filnavn.py $image
+  ln -s $image $IMAGE_FOLDER
+done
+
+for pdf_file in $pdf_files do;
+  ln -s $pdf_file $PDF_FOLDER
+done
+
+# Compress changed files.
+
+# Komprimer biletet
+# convert "$orgfil" -geometry 1024x1600  -quality 60 -compress JPEG -strip "$nyfil"
+# chmod a+r-x "$nyfil"
+
+# if [ ! -e "$nyfil" ]; then
+#   if [ -n "$(grep $orgfil error2)" ]; then
+#     #rm "$orgfil"
+#     echo "Tydelegvis problem m $orgfil..." >&2
+#     echo $orgfil >> persistent_error
+#     continue
+#   fi
+
+#   echo "E: convert \"$orgfil\" -geometry 1024x1600  -quality 60 -compress JPEG -strip  \"$nyfil\"" >&2
+#   echo "AVSLUTTA MED FEIL, konvertering mislukkast :("
+# echo "$orgfil" >> error
+# fi
