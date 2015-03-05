7b56a76c6a1691212e3580700faaa371ab4733ed
Håken Lid <haakenlid@gmail.com>
2015-03-05 14:45:40 +0100

    pdf cronjobb shellscript funker nå (helt sikkert) - eller klar til testing

--- cron/pdf_merge.sh
+++ cron/pdf_merge.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
 # load environmental variables.
-source ${0%/*}/cron_environment_variables.sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/cron_environment_variables.sh
+
+# SCRIPT_FOLDER=$DIR
+# PDF_FOLDER="/home/haakenlid/Desktop/PDF"
+# YEAR=2015
+# ISSUE=8
 
 cd "$PDF_FOLDER"
 mkdir -p "web"
-output_file="universitas-$YEAR-$ISSUE.pdf"
-color_profile="$SCRIPT_FOLDER/ISOnewspaper26v4.icc"
+output_file="web/universitas-$YEAR-$ISSUE.pdf"
+cmyk_profile="$SCRIPT_FOLDER/ISOnewspaper26v4.icc"
+rgb_profile="$SCRIPT_FOLDER/sRGB.icc"
 
 # Compile pdf files into multipage document.
+  # -dUseCIEColor=true \
+  # -dProcessColorModel=/DeviceRGB \
+  # -sDefaultCMYKProfile=$cmyk_profile \
 /usr/bin/gs \
-  -sDefaultCMYKProfile=$color_profile \
-  -dFastWebView=true \
-  -sDEVICE=pdfwrite  \
   -dBATCH  \
   -dNOPAUSE  \
+  -sDEVICE=pdfwrite  \
   -dCompatibilityLevel=1.4 \
+  -dFastWebView=true \
+  -sDefaultRGBProfile=$rgb_profile \
+  -dProcessColorModel=/DeviceRGB \
+  -dColorConversionStrategy=/sRGB \
+  -dColorConversionStrategyForImages=/sRGB \
   -dConvertCMYKImagesToRGB=true \
   -dDownsampleColorImages=true \
   -dDownsampleGrayImages=true \
@@ -23,7 +36,5 @@ color_profile="$SCRIPT_FOLDER/ISOnewspaper26v4.icc"
   -dColorImageResolution=120 \
   -dGrayImageResolution=120 \
   -dMonoImageResolution=120 \
-  -dProcessColorModel=/DeviceRGB \
-  -dUseCIEColor=true \
   -sOutputFile="$output_file" \
    UNI11VER*.pdf
--- cron/pdf_thumbs.sh
+++ cron/pdf_thumbs.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 
 # load environmental variables.
-source ${0%/*}/cron_environment_variables.sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/cron_environment_variables.sh
 
-# L=1200
-L=800
-M=500
-S=150
+size=800
+thumbs_folder="$PDF_FOLDER/thumbs"
+mkdir -p "$thumbs_folder"
 
 cd "$PDF_FOLDER"
-mkdir -p "png_$L"
-# mkdir -p "jpg_$L"
-# mkdir -p "jpg_$M"
-# mkdir -p "jpg_$S"
 
+set +x
 for pdf_file in UNI11VER*000.pdf; do
-  pngL=png_$L/$(echo "$pdf_file" | sed -r "s/UNI11VER.{6}(..)000.pdf/universitas-$YEAR-$ISSUE-page-\1.png/")
-  jpgL=$(echo "$pngL" | sed -r s/png/jpg/g)
-  jpgM=$(echo "$jpgL" | sed -r s/"$L"/"$M"/g)
-  jpgS=$(echo "$jpgL" | sed -r s/"$L"/"$S"/g)
-
-  if [[ -e "$pgnL" || "$pdf_file" -nt "$pngL" ]]; then
-  # echo "$pdf_file" "$pngL"
+  thumbfile="$thumbs_folder"/$(echo "$pdf_file"\
+    | sed -r "s/UNI11VER.{6}(..)000.pdf/universitas-$YEAR-nr$ISSUE-s\1.png/")
+  if [[ ! -f "$thumbfile" || "$pdf_file" -nt "$thumbfile" ]]; then
     convert \
       -density 160 \
-      -colorspace CMYK \
       "$pdf_file"\
       -background white \
       -flatten \
-      -resize "$L"x \
+      -resize "$size"x \
       -format png \
-      -colorspace sRGB \
-      "$pngL"
-
-    # convert "$pngL" -format jpg "$jpgL"
-    # convert "$pngL" -format jpg -resize "$M"x "$jpgM"
-    # convert "$pngL" -format jpg -resize "$S"x "$jpgS"
+      -colorspace RGB \
+      "$thumbfile"
   fi
 done
