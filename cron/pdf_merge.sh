b3e9ac41a8c700b023c1898e3fff6e1fa566c825
Håken Lid <haakenlid@gmail.com>
2015-03-25 10:16:38 +0100

    merge og thumbs for pdf

--- cron/pdf_merge.sh
+++ cron/pdf_merge.sh
@@ -4,6 +4,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/cron_environment_variables.sh
 
+echo $DIR $PDF_FOLDER $ISSUE
+
 # SCRIPT_FOLDER=$DIR
 # PDF_FOLDER="/home/haakenlid/Desktop/PDF"
 # YEAR=2015
@@ -15,20 +17,23 @@ output_file="web/universitas-$YEAR-$ISSUE.pdf"
 cmyk_profile="$SCRIPT_FOLDER/ISOnewspaper26v4.icc"
 rgb_profile="$SCRIPT_FOLDER/sRGB.icc"
 
+echo $output_file $cmyk_profile $rgb_profile
+
 # Compile pdf files into multipage document.
   # -dUseCIEColor=true \
   # -dProcessColorModel=/DeviceRGB \
   # -sDefaultCMYKProfile=$cmyk_profile \
+  # -dProcessColorModel=/DeviceRGB \
+  # -dColorConversionStrategy=/sRGB \
 /usr/bin/gs \
+  -dUseCIEColor=true \
+  -dProcessColorModel=/DeviceRGB \
+  -dColorConversionStrategyForImages=/DeviceRGB \
   -dBATCH  \
   -dNOPAUSE  \
   -sDEVICE=pdfwrite  \
   -dCompatibilityLevel=1.4 \
   -dFastWebView=true \
-  -sDefaultRGBProfile=$rgb_profile \
-  -dProcessColorModel=/DeviceRGB \
-  -dColorConversionStrategy=/sRGB \
-  -dColorConversionStrategyForImages=/sRGB \
   -dConvertCMYKImagesToRGB=true \
   -dDownsampleColorImages=true \
   -dDownsampleGrayImages=true \
--- cron/pdf_thumbs.sh
+++ cron/pdf_thumbs.sh
@@ -4,25 +4,25 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/cron_environment_variables.sh
 
-size=800
+size=1000
 thumbs_folder="$PDF_FOLDER/thumbs"
 mkdir -p "$thumbs_folder"
 
 cd "$PDF_FOLDER"
 
-set +x
 for pdf_file in UNI11VER*000.pdf; do
   thumbfile="$thumbs_folder"/$(echo "$pdf_file"\
-    | sed -r "s/UNI11VER.{6}(..)000.pdf/universitas-$YEAR-nr$ISSUE-s\1.png/")
+    | sed -r "s/UNI11VER.{6}(..)000.pdf/universitas-$YEAR-nr$ISSUE-s\1.jpg/")
   if [[ ! -f "$thumbfile" || "$pdf_file" -nt "$thumbfile" ]]; then
     convert \
       -density 160 \
+      -colorspace CMYK \
       "$pdf_file"\
       -background white \
       -flatten \
       -resize "$size"x \
-      -format png \
-      -colorspace RGB \
+      -format jpg \
+      -colorspace sRGB \
       "$thumbfile"
   fi
 done
