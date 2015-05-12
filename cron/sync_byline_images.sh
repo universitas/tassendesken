d4277e0d6d8d86ce64ee4a0236af5113b7a2bff8
Håken Lid <haakenlid@gmail.com>
2015-05-12 22:20:47 +0200

    sync funker

--- cron/sync_byline_images.sh
+++ cron/sync_byline_images.sh
@@ -7,14 +7,14 @@ echo scripts: $SCRIPT_FOLDER
 # logfile
 logfile="$STAGING/bylines.log"
 
-LOCAL_BYLINES=/uio/kant/div-universitas-desken/FOTO/byline-web
+LOCAL_BYLINES=/uio/kant/div-universitas-desken/FOTO/byline-web/
 REMOTE_BYLINES=haakenlid@universitas.no:/srv/fotoarkiv_universitas/byline/
 
-image_files=$(find "$LOCAL_BYLINES/" *.*)
+image_files=$(find "$LOCAL_BYLINES")
 
 # for image in $(ls $LOCAL_BYLINES); do
 for image in $image_files; do
   $SCRIPT_FOLDER/fix_filnavn.py $image
 done
 
-# /usr/bin/rsync -rthzvL $LOCAL_BYLINES $REMOTE_BYLINES
+/usr/bin/rsync -rthzvL $LOCAL_BYLINES $REMOTE_BYLINES
