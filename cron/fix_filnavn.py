59ac35caeed254a77fed4f071b14cd8f673eaaa9
Håken Lid <haakenlid@gmail.com>
2015-05-12 22:09:05 +0200

    commit for at sync endrer filnavn

--- cron/fix_filnavn.py
+++ cron/fix_filnavn.py
@@ -11,12 +11,12 @@ from unidecode import unidecode
 for myfile in sys.argv[1:]:
     path = myfile.split('/')
     new_name = unidecode(path[-1].decode('utf-8'))
+    new_name = path[-1]
     new_name = re.sub(r'[^A-Za-z0-9.]+', '-', new_name)
     new_name = re.compile(r'-*\.jpe?g', re.I).sub('.jpg', new_name)
     new_name = re.compile(r'-*\.png', re.I).sub('.png', new_name)
     path[-1] = new_name
-    new_path = '/'.join(path)
-    print(path, new_path)
+    new_path = u'/'.join(path)
     if new_path != myfile:
         shutil.move(myfile, new_path)
     print new_path
--- cron/sync_byline_images.sh
+++ cron/sync_byline_images.sh
@@ -2,6 +2,7 @@
 
 # load environmental variables.
 source ${0%/*}/cron_environment_variables.sh
+echo scripts: $SCRIPT_FOLDER
 
 # logfile
 logfile="$STAGING/bylines.log"
@@ -9,12 +10,11 @@ logfile="$STAGING/bylines.log"
 LOCAL_BYLINES=/uio/kant/div-universitas-desken/FOTO/byline-web
 REMOTE_BYLINES=haakenlid@universitas.no:/srv/fotoarkiv_universitas/byline/
 
-
 image_files=$(find "$LOCAL_BYLINES/" *.*)
 
-for image in $(ls $IMAGE_FOLDER); do
-  fixed=$($SCRIPT_FOLDER/fix_filnavn.py $image)
-  echo $image ">" $fixed
+# for image in $(ls $LOCAL_BYLINES); do
+for image in $image_files; do
+  $SCRIPT_FOLDER/fix_filnavn.py $image
 done
 
 # /usr/bin/rsync -rthzvL $LOCAL_BYLINES $REMOTE_BYLINES
