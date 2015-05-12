d937e9911a530052b04e8b04d48c93c781a1e4b1
Håken Lid <haakenlid@gmail.com>
2015-05-12 21:34:33 +0200

    fix filnavn mekkings

--- cron/fix_filnavn.py
+++ cron/fix_filnavn.py
@@ -7,17 +7,16 @@ from unidecode import unidecode
 # import os
 
 # Husk at folder må være en unicode-string for at filnavn også skal være det.
-# WATCH_FOLDER = u'/div/platon/universitas-f2/UTTEGNER/CRON/'
-
 
 for myfile in sys.argv[1:]:
     path = myfile.split('/')
     new_name = unidecode(path[-1].decode('utf-8'))
     new_name = re.sub(r'[^A-Za-z0-9.]+', '-', new_name)
-    new_name = re.compile(r'\.jpe?g', re.I).sub('.jpg', new_name)
-    new_name = re.compile(r'\.png', re.I).sub('.png', new_name)
+    new_name = re.compile(r'-*\.jpe?g', re.I).sub('.jpg', new_name)
+    new_name = re.compile(r'-*\.png', re.I).sub('.png', new_name)
     path[-1] = new_name
     new_path = '/'.join(path)
+    print(path, new_path)
     if new_path != myfile:
         shutil.move(myfile, new_path)
     print new_path
