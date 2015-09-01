8e8a8b6521cef9001ac04cc9a2d411006f89bcb2
Håken Lid <haakenlid@gmail.com>
2015-09-01 23:05:14 +0200

    fix filnavn takler unicode

--- cron/fix_filnavn.py
+++ cron/fix_filnavn.py
@@ -9,12 +9,12 @@ from unidecode import unidecode
 # Husk at folder må være en unicode-string for at filnavn også skal være det.
 
 for myfile in sys.argv[1:]:
-    path = myfile.split('/')
-    new_name = unidecode(path[-1].decode('utf-8'))
-    new_name = path[-1]
-    new_name = re.sub(r'[^A-Za-z0-9.]+', '-', new_name)
-    new_name = re.compile(r'-*\.jpe?g', re.I).sub('.jpg', new_name)
-    new_name = re.compile(r'-*\.png', re.I).sub('.png', new_name)
+    myfile = myfile.decode('utf-8')
+    path = myfile.split(u'/')
+    new_name = unidecode(path[-1])
+    new_name = re.sub(ur'[^A-Za-z0-9.]+', '-', new_name)
+    new_name = re.compile(ur'-*\.jpe?g', re.I).sub(u'.jpg', new_name)
+    new_name = re.compile(ur'-*\.png', re.I).sub(u'.png', new_name)
     path[-1] = new_name
     new_path = u'/'.join(path)
     if new_path != myfile:
