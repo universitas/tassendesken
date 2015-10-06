255d49b088e7adee565a6ae98eaaf538382231b7
Håken Lid <haakenlid@gmail.com>
2015-10-06 13:58:15 +0200

    feil variabelnavn

--- cron/local_backup.py
+++ cron/local_backup.py
@@ -17,38 +17,45 @@ ARKIV_IP = '129.240.79.50'
 ARKIV_LOGIN = 'dennis'
 ARKIV_SHARE = 'arkiv'
 
+
 def main():
+    """Backup current issue over sftp to external backup device"""
     year = datetime.datetime.now().year
     desken = desken_folder()
     issue = find_current_issue(desken)
     print desken, year, issue
     archive_files_over_sftp(
-        local_folder = '%s/%s/' % (desken, issue),
-        remote_folder = '%s/%s/%s' % (ARKIV_SHARE, year, issue)
+        local_folder='%s/%s/' % (desken, issue),
+        remote_folder='%s/%s/%s' % (ARKIV_SHARE, year, issue)
     )
+
 
 def desken_folder():
     """Walk up the path tree to find desken folder"""
     this_file = os.path.abspath(__file__)
-    up = os.path.dirname
+    up = os.path.dirname 
     desken = up(up(up(this_file)))
     # desken = "/uio/kant/div-universitas-desken/"
     return desken
+
 
 def find_current_issue(desken):
     """Find issue number based on which folder is the largest number"""
     folders = os.listdir(desken)
     try:
-        latest_issue = max(int(folder) for folder in folders if folder.isdigit())
+        latest_issue = max(int(folder)
+                           for folder in folders if folder.isdigit())
     except ValueError:
         exit('No issue folder found in %s' % desken)
     return str(latest_issue)
 
+
 def archive_files_over_sftp(local_folder, remote_folder):
     """Connect to SFTP server and copy local folder recursively"""
-    with pysftp.Connection(ARKIV_IP, username=ARKIV_LOGIN, password=ARKIV_LOGIN) as sftp:
-        sftp.makedirs(dst)
-        sftp.put_r(src, dst)
+    with pysftp.Connection(
+            ARKIV_IP, username=ARKIV_LOGIN, password=ARKIV_LOGIN) as sftp:
+        sftp.makedirs(remote_folder)
+        sftp.put_r(local_folder, remote_folder)
 
 if __name__ == '__main__':
     main()
