3d8dcbac28d46483f84cf4184857f5f8b532ee09
Håken Lid <haakenlid@gmail.com>
2015-10-06 18:13:49 +0200

    fix filnavn funker for mapper

--- cron/fix_filnavn.py
+++ cron/fix_filnavn.py
@@ -1,22 +1,52 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Recursively fix filenames to web friendly ascii instead of unicode"""
+from __future__ import print_function
 import re
 import shutil
 import sys
+import os
 from unidecode import unidecode
-# import os
 
-# Husk at folder må være en unicode-string for at filnavn også skal være det.
+def walk(filename):
+    if not os.path.exists(filename):
+        exit(u'File not found: %s' % filename)
+    branch, leaf = os.path.split(os.path.abspath(filename))
+    filename = rename(branch, leaf, unidecode_filename(leaf))
 
-for myfile in sys.argv[1:]:
-    myfile = myfile.decode('utf-8')
-    path = myfile.split(u'/')
-    new_name = unidecode(path[-1])
+    if os.path.isdir(filename):
+        for branch, _, leaves in os.walk(filename, topdown=False):
+            root, dirname = os.path.split(branch)
+            branch = rename(root, dirname, unidecode_filename(dirname))
+            for leaf in leaves:
+                rename(branch, leaf, unidecode_filename(leaf))
+
+
+def rename(root, old, new):
+    old_path = os.path.join(root, old)
+    new_path = os.path.join(root, new)
+    if new_path != old_path:
+        while os.path.exists(new_path):
+            new = '0' + new
+            new_path = os.path.join(root, new)
+        shutil.move(old_path, new_path)
+
+    print('{}\n{}\n'.format(old_path, new_path))
+    # return old_path
+    return new_path
+
+
+def unidecode_filename(filename):
+    filename = filename.decode('utf-8')
+    new_name = unidecode(filename)
     new_name = re.sub(ur'[^A-Za-z0-9.]+', '-', new_name)
+    new_name = new_name.strip('-')
+    new_name = re.sub(ur'-?\.-?', '.', new_name)
     new_name = re.compile(ur'-*\.jpe?g', re.I).sub(u'.jpg', new_name)
     new_name = re.compile(ur'-*\.png', re.I).sub(u'.png', new_name)
-    path[-1] = new_name
-    new_path = u'/'.join(path)
-    if new_path != myfile:
-        shutil.move(myfile, new_path)
-    print new_path
+    return new_name.encode('utf-8')
+
+
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        walk(filename)
