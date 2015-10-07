652ff695b2184b70409e4daddf2e0493afeb840b
Håken Lid <haakenlid@gmail.com>
2015-10-07 11:47:22 +0200

    s3upload

--- cron/S3_upload.py
+++ cron/S3_upload.py
@@ -8,24 +8,27 @@ import sys
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
-AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
-
+AWS_BUCKET_NAME = 'static.universitas.no'
+BUCKET_ROOT_DIR = 'arkiv'
+AWS_BUCKET_HOST = 's3.eu-central-1.amazonaws.com'
+HEADERS = {'cache-control': 'max-age=1296000'}
 
 def upload_files_from(sourcedir, bucket_name, aws_key_id, aws_key, ):
     """Crawl sourcedir and upload all files to bucket"""
-    connection = boto.connect_s3(aws_key_id, aws_key)
+    os.environ['S3_USE_SIGV4'] = 'True'
+    connection = boto.connect_s3(aws_key_id, aws_key, host=AWS_BUCKET_HOST)
     bucket = connection.get_bucket(bucket_name)
-
-    print(sourcedir, bucket_name, aws_key, aws_key_id)
 
     for (root, _, filenames) in os.walk(sourcedir):
         for filename in filenames:
-            dst = os.path.join(root, filename)
-            src = os.path.abspath(dst)
+            dst = os.path.join(BUCKET_ROOT_DIR, root, filename)
+            src = os.path.abspath(os.path.join(root, filename))
             key = boto.s3.key.Key(bucket)
             key.key = dst
-            key.set_contents_from_filename(src)
-            print("   {src}\n-> {dst}\n".format(src=src, dst=dst))
+            key.set_contents_from_filename(src, headers=HEADERS)
+            print("{src}\n-> {bucket} /{dst}\n".format(
+                bucket=bucket_name, src=src, dst=dst))
+
 
 if __name__ == '__main__':
     for sourcedir in sys.argv[1:]:
