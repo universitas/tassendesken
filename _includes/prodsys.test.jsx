891c36175a2d88893e42ed3e86d9fbb6aa5c325c
Håken Lid <haakenlid@gmail.com>
2017-08-20 01:09:34 +0200

    tweak prodsys

---
+++ _includes/prodsys.test.jsx
@@ -0,0 +1,19 @@
+﻿#include "../_includes/config.jsxinc"; // diverse konfigurasjonsverdier
+#include "../_includes/dokTools.jsxinc"; // diverse nyttige verktøyfunksjoner
+#include "../_includes/prodsys.jsxinc"; // kommunikasjon med prodsys
+#include "../_includes/json2.jsxinc"; // json library
+
+main(62496)
+
+function log(message) {
+    // log message
+    if (typeof message === 'object') 
+      message = JSON.stringify(message, null, 2);
+    $.writeln(message);
+}
+
+
+function main(pk){
+    var sak = prodsys.get(pk);
+    log(sak.tekst);
+}
