b8fb0e1e1ec76e2d3a5bd94b60aed8e007969764
Håken Lid <haakenlid@gmail.com>
2018-05-24 18:54:47 +0200

    undomodes

---
+++ NEW/http_indesign.jsx
@@ -0,0 +1,27 @@
+#target indesign
+#targetengine session
+
+function script(fileName) {
+  // return script file as string for bridgetalk
+  var scriptFile = File(File($.fileName).parent + '/' + fileName)
+  if (!scriptFile.exists)
+    return 'File ' + scriptFile.fullName + ' does not exists'
+  scriptFile.open()
+  var content = scriptFile.read()
+  scriptFile.close()
+  return content
+}
+
+function get(url, callback) {
+    // http get
+    var bt = new BridgeTalk();
+    bt.target = BridgeTalk.getSpecifier('bridge');
+    s = script('bridge_http.jsx');
+    s += '\nget("' + url + '")';
+    bt.body = s;
+    bt.onResult = callback;
+    bt.onError = callback;
+    bt.send();
+};
+
+get('http://universitas.no/api/', function (ev) { $.writeln(ev.body) } )
