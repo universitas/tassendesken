1ca5d16c466b3aff801b6f8c069ee3f2dc0dbafa
Håken Lid <haakenlid@gmail.com>
2018-11-27 22:26:08 +0100

    async page exported should work

--- work_in_progress/async_page_export.jsx
+++ work_in_progress/async_page_export.jsx
@@ -2,6 +2,10 @@
 #targetengine "session"
 #include ../_includes/index.jsxinc
 
+if (ifMain($.fileName)) main()
+var FILEPATTERN = /^UNI.*.indd$/
+FILEPATTERN = /.indd$/
+
 function exportToFile(doc, page, format) {
   var directory = new Folder(doc.filePath.path + '/PREVIEW/')
   directory.exists || directory.create()
@@ -9,17 +13,19 @@ function exportToFile(doc, page, format) {
 }
 
 function export_all_pages_as_pdf(doc, syncronous) {
-  var preset = app.pdfExportPresets[6].duplicate()
-  // app.pdfExportPreferences.includeSlugWithPDF = true
-  preset.includeSlugWithPDF = true
+  var preset = app.pdfExportPresets.itemByName('UNIVERSITAS')
+  if (!preset.isValid) preset = app.pdfExportPresets[-1]
+  app.pdfExportPreferences.includeSlugWithPDF = true
+  var files = []
   for (var i = 0; i < doc.pages.length; i++) {
     var page = doc.pages[i]
     var file = exportToFile(doc, page.name, 'pdf')
     app.pdfExportPreferences.pageRange = page.name
     if (syncronous) doc.exportFile(ExportFormat.PDF_TYPE, file, false, preset)
     else doc.asynchronousExportFile(ExportFormat.PDF_TYPE, file, false, preset)
+    files.push(file)
   }
-  preset.remove()
+  return files
 }
 
 function export_all_pages_as_jpeg(doc) {
@@ -29,25 +35,43 @@ function export_all_pages_as_jpeg(doc) {
   expPrefs.useDocumentBleeds = true
   expPrefs.exportingSpread = false
   expPrefs.exportResolution = 150
+  var files = []
   for (var i = 0; i < doc.pages.length; i++) {
     var page = doc.pages[i]
     var file = exportToFile(doc, page.name, 'jpg')
     expPrefs.pageString = page.name
     doc.exportFile(ExportFormat.JPG, file, false)
+    files.push(file)
   }
+  log('foo')
+  return files
 }
 
 function postSaveExport(event) {
   var doc = event.target
   $.writeln(doc)
-  if (!(doc.constructor.name == 'Document' && doc.name.match(/UNI.*.indd/))) {
+  if (!(doc.constructor.name == 'Document' && doc.name.match(FILEPATTERN))) {
+    $.writeln('didnt match')
     return
   }
-  export_all_pages_as_pdf(doc)
+  tryLogErrors(export_all_pages_as_pdf)(doc)
 }
 
-// app.addEventListener('beforeSave', postSaveExport)
-// timeIt(export_all_pages_as_jpeg)(app.activeDocument)
-timeIt(export_all_pages_as_pdf)(app.activeDocument, true)
-// export_all_pages_as_jpeg(app.activeDocument)
+function removeEventListeners(eventType) {
+  // detach all event listener of type
+  return pipe(
+    prop('eventListeners'),
+    filter(propEq('eventType', eventType)),
+    map(call('remove')),
+    prop('length'),
+  )(app)
+}
+
+function main() {
+  removeEventListeners(DocumentEvent.BEFORE_SAVE)
+  app.addEventListener(DocumentEvent.BEFORE_SAVE, postSaveExport)
+  $.writeln('added event listener')
+  postSaveExport({target: app.activeDocument})
+}
+
 // vi: ft=javascript
---
+++ work_in_progress/list-attached-eventlisteners.jsx
@@ -0,0 +1,12 @@
+#include ../_includes/index.jsxinc
+
+function removeEventListeners(eventType) {
+  return pipe(
+    prop('eventListeners'),
+    filter(propEq('eventType', eventType)),
+    map(call('remove')),
+    prop('length'),
+  )(app)
+}
+
+removeEventListeners(DocumentEvent.BEFORE_SAVE)
