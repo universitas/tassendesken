74d394807b67cccf2debf8662191d78511e489ee
Håken Lid <haakenlid@gmail.com>
2018-11-28 07:01:31 +0100

    refactor saksmaler script and improve performance

--- indesign/legg_Saksmaler_i_Bibliotek.jsx
+++ indesign/legg_Saksmaler_i_Bibliotek.jsx
@@ -1,138 +1,136 @@
 // Lager saksmaler fra dokumentet saksmaler.indd
 // Legger malgrupper til i et bibliotek og oppretter MAL_AVIS.indt
-// Skrevet av Håken Lid 2011
 
 #target "indesign"
 #targetengine "session"
 #include ../_includes/index.jsxinc
-dokTools.clearSearch()
 
-var lagreOverAvismal = true // lagrer også ny MAL_AVIS
+//~ var myLibrary = File(config.saksmalLibrary) // Biblioteket som saksmalene skal legges til
+//~ var lagreSom = File(config.mal_avis) // Avismalen
 
-var myLibrary = File(config.saksmalLibrary) // Biblioteket som saksmalene skal legges til
-var lagreSom = File(config.mal_avis) // Avismalen
+var libraryFile = File('saksmaler.indl')
+var lagreSom = File('MAL.indt')
+var annotation = 'beskjed'
 
-var doc = app.activeDocument
-var win = doc.layoutWindows[0]
-var annotation = dokTools.velgStil(doc, 'object', 'beskjed')
-var parStyle1 = dokTools.velgStil(doc, 'paragraph', 'A 5påplassen navn')
-var parStyle2 = dokTools.velgStil(doc, 'paragraph', 'A 5påplassen')
-var myReportTextFrame
+if (ifMain($.fileName)) main()
 
-if (!alleGrupper) {
-  mekkMalGeo(app.selection, promptforLabel)
-} else if (
-  confirm(
-    'Saksmaler\nVil du legge alle saksmaler inn i bibiliteket Saksmaler.indl?',
+function clearReports(doc, objStyle) {
+  app.findObjectPreferences = NothingEnum.nothing
+  app.findObjectPreferences.appliedObjectStyles = doc.objectStyles.itemByName(
+    objStyle
+  )
+  map(call('remove'))(doc.findObject())
+}
+
+function findTemplateGroups(doc) {
+  return pipe(
+    prop('pageItems'),
+    filter(prop('label')),
+    filter(either(is(TextFrame), is(Group))),
+    filter(propEq('parentPage.parent.constructor.name', 'Spread')),
+    sort(
+      ascend(
+        pipe(
+          dotProp('parentPage.name'),
+          parseFloat
+        )
+      )
+    )
+  )(doc)
+}
+
+function itemName(item) {
+  var gb = item.geometricBounds
+  var width = gb[3] - gb[1]
+  var height = gb[2] - gb[0]
+  var cols = Math.ceil(width / 57)
+  var withLoft = gb[0] > 70 && height > 250 ? ' loft' : ''
+  return titleCase(item.label) + ' ' + cols + ' spalter' + withLoft
+}
+
+function main() {
+  var step0 = confirm(
+    'Vil du legge alle saksmaler inn i biblioteket Saksmaler.indl?',
+    false,
+    'Saksmaler'
+  )
+  var step1 = confirm(
+    'Er du sikker på at du vil lagre ny MAL_AVIS.indt?',
+    false,
+    'Lagre over'
+  )
+
+  if (step0)
+    app.doScript(
+      buildLibrary,
+      undefined,
+      undefined,
+      UndoModes.ENTIRE_SCRIPT,
+      'legg saksmaler'
+    )
+  if (step1)
+    app.doScript(
+      lagNyAvisMal,
+      undefined,
+      undefined,
+      UndoModes.FAST_ENTIRE_SCRIPT,
+      'lag mal'
+    )
+}
+
+function prepareLibrary(libraryFile) {
+  backupFile(libraryFile)
+  map(call('close'))(app.libraries)
+  app.libraries.add(libraryFile)
+  myLibrary = app.open(libraryFile)
+  app.panels.itemByName(myLibrary.name.split('.')[0]).visible = false
+  myLibrary.assets.everyItem().remove()
+  return myLibrary
+}
+
+function buildLibrary() {
+  dokTools.zoomOut()
+  var myProgressBar = dokTools.progressBar(
+    'Legger maler til ' + libraryFile.name,
+    'Gjør klar',
+    120,
     false
   )
-) {
-  if (addToLibrary && myLibrary.exists) {
-    win.zoom(ZoomOptions.fitSpread)
-    myProgressBar = dokTools.progressBar(
-      'Legger maler til ' + myLibrary.name,
-      'Gjør klar',
-      120,
-      false
-    )
-    app.libraries.everyItem().close()
-    backupFile(myLibrary)
-    myLibrary = app.open(myLibrary)
-    app.panels.itemByName('Saksmaler').visible = false
-    myLibrary.assets.everyItem().remove()
-  }
+  var doc = app.activeDocument
   clearReports(doc, annotation)
-  var myItem
-  var mySpread
-  var itemName
-  myProgressBar.update('teller grupper', 10)
-  var myPageItems = doc.pageItems.everyItem().getElements()
-  var myItems = []
-  var myPage
-  for (i = myPageItems.length - 1; i > -1; i--) {
-    myProgressBar.update('teller grupper')
-    myItem = myPageItems[i]
-    if (!dokTools.onPage(myItem)) continue;
-    mySpread = myItem
-    if (myItem instanceof Group || myItem instanceof TextFrame) {
-      while (
-        false ===
-        (mySpread instanceof Spread ||
-          mySpread instanceof MasterSpread ||
-          mySpread instanceof Application)
-      ) {
-        mySpread = mySpread.parent
-      }
-      if (mySpread instanceof Spread) {
-        myPage = myItem.parent
-        if (myPage instanceof Spread) {
-          myPage = myPage.pages[0]
-        }
-        myItems.push([myPage, myItem])
-      }
-    }
-  }
+  var myLibrary = prepareLibrary(libraryFile)
+  var myItems = findTemplateGroups(app.activeDocument)
   myProgressBar.update('Finner saker', Math.max(0, 120 - myItems.length))
-  myItems.sort(function(a, b) {
-    return b[0].name - a[0].name
-  })
-  for (var i = myItems.length - 1; i >= 0; i--) {
-    myItem = myItems[i][1]
-    myPage = myItems[i][0]
-    app.select(myItem)
-    dokTools.zoomTo(myItem)
-
-    if (myItem.label !== '') {
-      var svar = mekkMalGeo([myItem], false)
-      if (svar === undefined) {
-        continue;
-      }
-      myItem = svar[0]
-      itemName = svar[1].split('\r')[0].replace(/\s/g, ' ')
-      myProgressBar.update('side ' + myPage.name + '  ' + itemName)
-      if (addToLibrary && myLibrary.assets.itemByName(itemName) === null) {
-        var myAsset = myItem.store(myLibrary)
-        myAsset.assetType = AssetType.TEXT_TYPE
-        myAsset.name = itemName
-        myAsset.description = svar[1]
-          .split('\r')
-          .slice(0, 2)
-          .join(' ')
-        myAsset.label = myReport
-      }
-    } else {
+  for (var i = 0; i < myItems.length; i++) {
+    var myItem = myItems[i]
+    var page = myItems[i].parentPage.name
+    var name = itemName(myItem)
+    myProgressBar.update('side ' + page + '  ' + name)
+    if (myLibrary.assets.itemByName(name) == null) {
       dokTools.zoomTo(myItem)
-      itemName = prompt('Malnavn? ')
-      // itemName = '[' + myItem.constructor.name + '] id# ' + myItem.id
-      myProgressBar.update('side ' + myPage.name + '  ' + itemName)
+      var properties = {
+        description: name,
+        name: name,
+        assetType: AssetType.TEXT_TYPE
+      }
+      myItem.store(myLibrary, properties)
     }
-    app.select(null)
-    // app.scriptPreferences.enableRedraw = false
   }
   myLibrary.close()
-  myProgressBar.close()
-  win.activePage = app.activeDocument.pages[0]
-}
-
-if (lagreOverAvismal) {
   dokTools.zoomOut()
-  if (
-    confirm(
-      'Lagre over?\rEr du sikker på at du vil lagre ny MAL_AVIS.indt ?',
-      false
-    )
-  ) {
-    lagNyAvisMal()
-  }
+  myProgressBar.close()
 }
 
-function lagNyAvisMal(doc) {
+function lagNyAvisMal() {
   var avismal = config.avismal
+  var doc = app.activeDocument
+  var win = app.activeWindow
   var myMasterPage
   if (!doc.name.match(/saksmaler/i)) {
     alert('feil fil', 'Kan bare lage avismal fra saksmaler.indd')
     return
   }
+  dokTools.zoomOut()
   var myProgressBar = dokTools.progressBar(
     'Legger maler til ' + lagreSom.name,
     'Lagrer Saksmaler.indd',
@@ -144,41 +142,27 @@ function lagNyAvisMal(doc) {
 
   for (var n = 0; n < avismal.length; n++) {
     myProgressBar.update('Lager oppslag: ' + (n + 1) + ' ' + avismal[n].master)
-    if (doc.spreads[n] !== null) {
-      win.activePage = doc.spreads[n].pages[-1]
-    } else {
-      win.activePage = doc.spreads[-1].pages[-1]
-    }
-    $.sleep(100)
     myMasterPage = doc.masterSpreads.itemByName(avismal[n].master)
-    if (myMasterPage === null) {
-      myMasterPage = doc.masterSpreads[0]
-    }
+    win.activePage = doc.spreads[n].pages[0]
+    if (!myMasterPage.isValid) myMasterPage = doc.masterSpreads[0]
     if (avismal[n].tomside === false) {
-      try {
-        while (
-          doc.spreads[n].appliedMaster !== myMasterPage &&
-          doc.spreads[n] !== null
-        ) {
-          doc.spreads[n].remove()
-        }
-      } catch (e) {
-        doc.spreads.add()
-        doc.spreads[-1].appliedMaster = myMasterPage
-      }
-    } else {
-      doc.spreads.add(LocationOptions.BEFORE, doc.spreads[n])
-      doc.spreads[n].appliedMaster = myMasterPage
-    }
+      var remove = 0
+      while (doc.spreads[n + remove].appliedMaster !== myMasterPage) remove++
+      if (remove) doc.spreads.itemByRange(n, n - 1 + remove).remove()
+    } else
+      doc.spreads.add(LocationOptions.BEFORE, doc.spreads[n], {
+        appliedMaster: myMasterPage
+      })
   }
   myProgressBar.update('Tar backup av AVIS_MAL.indt')
-  win.activePage = doc.pages[0]
   backupFile(lagreSom)
   myProgressBar.update('Lagrer ny AVIS_MAL.indt')
+  var original = doc.fullName
   doc.save(lagreSom, true, '', true)
   doc.close()
   myProgressBar.update('Ferdig!')
-  $.sleep(1000)
+  app.open(original)
+  dokTools.zoomOut()
   myProgressBar.close()
 }
 // vi: ft=javascript
